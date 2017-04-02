7f15b01e569338eaaf23832c57e673b265024b1a
PhilippeH1967 <phhaumesser@gmail.com>
2017-04-02 10:15:40 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,17 +1,31 @@
 # Reading, naming and subsetting power consumption data
-power <- read.table("household_power_consumption.txt",skip=1,sep=";")
-names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+pdata1 <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
 
-# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
-subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
-subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
-subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
-subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+# change date 1. et 2. Februar 2007
 
+data2 <- subset(data1, Date == "1/2/2007" | Date == "2/2/2007")
 
-# calling the basic plot function
-plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+# Ddate gestion
 
-# annotating graph
-title(main="Global Active Power Vs Time")
+data2$timestamp <- paste(data2$Date, data2$Time)
+data2$timestamp <- strptime(data2$timestamp, "%d/%m/%Y %H:%M:%OS")
+
+data2$timestamp <- as.POSIXlt(data2$timestamp)
+data2$weekday <- weekdays(data2$timestamp)
+
+# as.numeric...
+
+data3 <- data2
+
+data3$Global_active_power <- as.numeric(data3$Global_active_power)
+data3$Sub_metering_1 <- as.numeric(data3$Sub_metering_1)
+data3$Sub_metering_2 <- as.numeric(data3$Sub_metering_2)
+data3$Sub_metering_3 <- as.numeric(data3$Sub_metering_3)
+data3$Voltage <- as.numeric(data3$Voltage)
+data3$Global_reactive_power <- as.numeric(data3$Global_reactive_power)
+
+## plot 2
+
+png(filename = "plot2.png", height = 480, width = 480)
+plot(data3$timestamp, data3$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
