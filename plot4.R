acfe62c641d3dae289be5c9b38ba10e09da539eb
PhilippeH1967 <phhaumesser@gmail.com>
2017-04-02 10:13:18 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,27 +1,45 @@
 ### Generating Plot 4
-# Reading, naming and subsetting power consumption data
-power <- read.table("household_power_consumption.txt",skip=1,sep=";")
-names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+# gestio ndes dates
 
-# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
-subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
-subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
-subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
-subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+data1 <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
 
+# subset für den 1. und 2. Februar 2007
 
-# initiating a composite plot with many graphs
+data2 <- subset(data1, Date == "1/2/2007" | Date == "2/2/2007")
+
+# 
+
+data2$timestamp <- paste(data2$Date, data2$Time)
+data2$timestamp <- strptime(data2$timestamp, "%d/%m/%Y %H:%M:%OS")
+
+data2$timestamp <- as.POSIXlt(data2$timestamp)
+data2$weekday <- weekdays(data2$timestamp)
+
+# as.numeric...
+
+data3 <- data2
+
+data3$Global_active_power <- as.numeric(data3$Global_active_power)
+data3$Sub_metering_1 <- as.numeric(data3$Sub_metering_1)
+data3$Sub_metering_2 <- as.numeric(data3$Sub_metering_2)
+data3$Sub_metering_3 <- as.numeric(data3$Sub_metering_3)
+data3$Voltage <- as.numeric(data3$Voltage)
+data3$Global_reactive_power <- as.numeric(data3$Global_reactive_power)
+
+## plot 4
+
+png(filename = "plot4.png", height = 480, width = 480)
 par(mfrow=c(2,2))
 
-# calling the basic plot function that calls different plot functions to build the 4 plots that form the graph
-with(subpower,{
-  plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",  xlab="",ylab="Global Active Power")  
-  plot(subpower$Time,as.numeric(as.character(subpower$Voltage)), type="l",xlab="datetime",ylab="Voltage")
-  plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
-   with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
-   with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
-   with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
-   legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.6)
-  plot(subpower$Time,as.numeric(as.character(subpower$Global_reactive_power)),type="l",xlab="datetime",ylab="Global_reactive_power")
-})
+plot(data3$timestamp, data3$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+plot(data3$timestamp, data3$Voltage, type = "l", xlab = "daytime", ylab = "Voltage")
+
+plot(data3$timestamp, data3$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(data3$timestamp, data3$Sub_metering_2, type = "l", col = "red")
+lines(data3$timestamp, data3$Sub_metering_3, type = "l", col = "blue")
+legend("topright", fill = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+plot(data3$timestamp, data3$Global_reactive_power, type = "l", xlab = "daytime", ylab = "Global_reactive_power")
+
+dev.off()
