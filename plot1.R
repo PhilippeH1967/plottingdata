a101c8f7114555eb3b3ea47f26dc80628f02881e
PhilippeH1967 <phhaumesser@gmail.com>
2017-04-02 10:16:42 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,31 @@
 #Reading, naming and subsetting power consumption data
-power <- read.table("household_power_consumption.txt",skip=1,sep=";")
-names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+data1 <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
 
-#calling the basic plot function
-hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+# gestion periode 1. et 2. Februar 2007
 
-# annotating graph
-title(main="Global Active Power")
+data2 <- subset(data1, Date == "1/2/2007" | Date == "2/2/2007")
+
+# Dates
+
+data2$timestamp <- paste(data2$Date, data2$Time)
+data2$timestamp <- strptime(data2$timestamp, "%d/%m/%Y %H:%M:%OS")
+
+data2$timestamp <- as.POSIXlt(data2$timestamp)
+data2$weekday <- weekdays(data2$timestamp)
+
+# as.numeric...
+
+data3 <- data2
+
+data3$Global_active_power <- as.numeric(data3$Global_active_power)
+data3$Sub_metering_1 <- as.numeric(data3$Sub_metering_1)
+data3$Sub_metering_2 <- as.numeric(data3$Sub_metering_2)
+data3$Sub_metering_3 <- as.numeric(data3$Sub_metering_3)
+data3$Voltage <- as.numeric(data3$Voltage)
+data3$Global_reactive_power <- as.numeric(data3$Global_reactive_power)
+
+## plot 1
+
+png(filename = "plot1.png", height = 480, width = 480)
+hist(data3$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
